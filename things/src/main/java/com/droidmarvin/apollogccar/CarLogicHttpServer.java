249131025ddf0fbd4e3319e076f0b2b1fb744ec1
ngesa marvin <marvinngesa@gmail.com>
2018-01-23 02:03:26 +0300

    glue all the classes in MainActivity

--- things/src/main/java/com/droidmarvin/apollogccar/CarLogicHttpServer.java
+++ things/src/main/java/com/droidmarvin/apollogccar/CarLogicHttpServer.java
@@ -1,4 +1,54 @@
 package com.droidmarvin.apollogccar;
 
-public class CarLogicHttpServer {
+import android.content.Context;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.Map;
+import fi.iki.elonen.NanoHTTPD;
+
+import static android.content.ContentValues.TAG;
+
+public class CarLogicHttpServer extends NanoHTTPD {
+
+    public static interface CommandListener { public void onCommand(String command);}
+
+    public void CarLogicHttpServer (int port, Context context, CommandListener listener) {
+        super(port);
+
+        this.context = context;
+        this.listener = listener;
+
+        Log.d(TAG, "Starting Server");
+
+        try {
+            start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+//    @Override
+//    public NanoHTTPD.Response serve(NanoHTTPD.IHTTPSession session) {
+//        Map<String, String> params = session.getParms();
+//        String control = params.get("control");
+//        String action = params.get("btn");
+//        Log.d(TAG, "Serve - Control ["+control+"] - Action ["+action+"]");
+//        if (action != null && !"".equals(action))
+//            listener.onCommand(action);
+//        return newFixedLengthResponse(readHTMLFile().toString());
+//    }
+
+    @Override
+    public Response serve(IHTTPSession session) {
+        String msg = "<html><body><h1>Hello server</h1>\n";
+        Map<String, String> parms = session.getParms();
+        if (parms.get("username") == null) {
+            msg += "<form action='?' method='get'>\n  <p>Your name: <input type='text' name='username'></p>\n" + "</form>\n";
+        } else {
+            msg += "<p>Hello, " + parms.get("username") + "!</p>";
+        }
+        return newFixedLengthResponse(msg + "</body></html>\n");
+
+    }
 }
--- things/src/main/java/com/droidmarvin/apollogccar/MainActivity.java
+++ things/src/main/java/com/droidmarvin/apollogccar/MainActivity.java
@@ -2,6 +2,7 @@ package com.droidmarvin.apollogccar;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
@@ -12,5 +13,25 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         final MotorLogic motorLogic = new MotorLogic();
+
+        CarLogicHttpServer server = new CarLogicHttpServer(8090, this,
+                new CarLogicHttpServer().CommandListener (){
+
+                    @Override
+                    public void onCommand(String command) {
+                        Log.d(TAG, "Command received ["+command+"]");
+                        if (command.equals("F"))
+                            motorLogic.forward();
+                        else if (command.equals("B"))
+                            motorLogic.backward();
+                        else if (command.equals("S"))
+                            motorLogic.stop();
+                        else if (command.equals("L"))
+                            motorLogic.turnLeft();
+                        else if (command.equals("R"))
+                            motorLogic.turnRight();
+                    }
+
+                });
     }
 }
