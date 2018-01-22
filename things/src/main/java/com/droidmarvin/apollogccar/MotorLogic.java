531fc8e901b1fe2903c06465eeb5ed075ade706a
ngesa marvin <marvinngesa@gmail.com>
2018-01-23 01:22:04 +0300

    setup the motor connection -- Live

--- things/src/main/java/com/droidmarvin/apollogccar/MotorLogic.java
+++ things/src/main/java/com/droidmarvin/apollogccar/MotorLogic.java
@@ -1,9 +1,35 @@
 package com.droidmarvin.apollogccar;
 
-/**
- * Created by Ngesa on 1/23/2018.
- */
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.PeripheralManagerService;
+
+import java.io.IOException;
 
 public class MotorLogic {
+
+    private Gpio pin1Motor1;
+    private Gpio pin2Motor1;
+
+    private Gpio pin1Motor2;
+    private Gpio pin2Motor2;
+
+    public void MotorController() {
+
+        PeripheralManagerService service = new PeripheralManagerService();
+
+        try {
+            // Right
+            pin1Motor1 = service.openGpio("BCM17");
+            pin2Motor1 = service.openGpio("BCM27");
+
+            // Left
+            pin1Motor2 = service.openGpio("BCM23");
+            pin2Motor2 = service.openGpio("BCM24");
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
